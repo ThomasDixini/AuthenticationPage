6742afdee55da05b6893dc55fff5dceb527aa65f
Thomás Dixini <thomasdixini@gmail.com>
2023-10-30 21:57:34 -0300

    :sparkles: feat: SubHeading da página feito

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import logo from '../assets/logo.png'
 
 export function Header() {
     return (
         <header className="w-full px-28 py-4 justify-center border-b-2 border-blue-800">
             <div className="max-w-7xl flex justify-between mx-auto items-center">
-                <Image width={100} height={100} alt="" src=""/>
+                <Image width={100} height={100} alt="" src={logo}/>
                 <button className="h-12 px-8 rounded font-bold bg-blue-600 flex items-center hover:bg-blue-900 duration-200">
                     Contate-nos
                 </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 export default function Home() {
   return (
-    <>
+    <div className="max-w-7xl m-auto ">
+      <header className="border-b-2 border-blue-800 py-8">
+        <h1 className="text-4xl font-bold"> KYC Verification </h1>
+        <span className="text-lg text-gray-400 opacity-70 leading-10">Verify your identity and get started </span>
+      </header>
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24`}
       >
-        <h1 className="text-6xl"> Hello world </h1>
+        
       </main>
-    </>
+
+      <aside>
+        
+      </aside>
+    </div>
   )
 }

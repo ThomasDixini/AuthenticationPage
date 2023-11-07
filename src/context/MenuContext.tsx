9936b7d61e880be45fca83b1827d2730521bb0eb
Thomás Dixini <thomasdixini@gmail.com>
2023-11-06 22:40:29 -0300

    :sparkles: feat: Decrementando currentStep

--- src/context/MenuContext.tsx
+++ src/context/MenuContext.tsx
@@ -7,22 +7,30 @@ interface MenuContextProviderProps {
 interface MenuContextData {
     currentStep: number
     handleAddCurrentStep: () => void
+    handleDecrementCurrentStep: () => void
 }
 
 export const MenuContext = createContext({} as MenuContextData)
 
 export function MenuContextProvider({children}: MenuContextProviderProps){
-    const [currentStep, setCurrentStep] = useState(2);
+    const [currentStep, setCurrentStep] = useState(1);
     const [complete, setComplete] = useState(false);
 
     function handleAddCurrentStep() {
         setCurrentStep(state => state += 1)
     }
 
+    function handleDecrementCurrentStep() {
+        if(currentStep > 1){
+            setCurrentStep(state => state -= 1)
+        }
+    }
+
     return(
         <MenuContext.Provider value={{
             currentStep, 
-            handleAddCurrentStep
+            handleAddCurrentStep,
+            handleDecrementCurrentStep
         }}>
             {children}
         </MenuContext.Provider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { HeaderSteps } from "@/components/HeaderSteps";
 import { MenuProgress } from "@/components/MenuProgress";
 import { ValidationFields } from "@/components/ValidationFields";
+import { MenuContext } from "@/context/MenuContext";
+import { useContext } from "react";
 
 export default function Home() {
+  const { handleAddCurrentStep, handleDecrementCurrentStep } = useContext(MenuContext)
+
   return (
     <div className="max-w-7xl m-auto grid grid-cols-3 flex">
       <header className="border-b-2 border-blue-800 py-8 col-span-3">
@@ -20,8 +24,8 @@ export default function Home() {
           <HeaderSteps />
           <ValidationFields />
           <div className="mt-8 flex justify-end">
-            <button className="px-12 h-14 font-bold hover:brightness-50 duration-200"> Back </button>
-            <button className="px-12 h-14 bg-blue-600 rounded-lg font-bold hover:bg-blue-900 duration-200"> Next </button>
+            <button className="px-12 h-14 font-bold hover:brightness-50 duration-200" onClick={handleDecrementCurrentStep}> Back </button>
+            <button className="px-12 h-14 bg-blue-600 rounded-lg font-bold hover:bg-blue-900 duration-200" onClick={handleAddCurrentStep}> Next </button>
           </div>
         </main>
       </div>
